a6726daadcf63bb52f6846381f330898e84557aa
Alexandr <87748202+shadowHD-bit@users.noreply.github.com>
2023-04-13 05:01:56 +0700

    fix: fixed often question page

--- client/src/pages/QuestionPage/QuestionPage.jsx
+++ client/src/pages/QuestionPage/QuestionPage.jsx
@@ -13,36 +13,59 @@ import { useEffect } from "react";
 import { fetchRules } from "../../http/rulesAPI";
 
 export default function () {
+  const [question, setQuestion] = useState([]);
 
-    const [question, setQuestion] = useState([])
-
-    useEffect(() => {
-        fetchRules().then(data => {
-            setQuestion(data.rows)
-        })
-    }, [])
+  useEffect(() => {
+    fetchRules().then((data) => {
+      setQuestion(data.rows);
+    });
+  }, []);
 
   return (
-    <div className="rules">
-      <div className="main_info">
-        <Container>
+    <>
+      <Container fluid="md">
+        <Row className="mb-5">
+          <Col
+            xs={12}
+            md={4}
+            className="d-lg-flex d-none flex-row justify-content-end"
+          >
+            <img
+              width={200}
+              src={process.env.PUBLIC_URL + "/img/stickers/coffee-time.png"}
+              alt="coffee-time"
+            />
+          </Col>
+          <Col
+            xs={12}
+            md={12}
+            lg={5}
+            className="d-flex flex-column justify-content-start align-items-center"
+          >
+            <p className="question_title">
+              <span className="red">Дорогой </span> пользователь!
+            </p>
+            <p>
+              На данной странице вы можете ознакомиться с частозадаваемыми вопросами. Советуем изучить данные вопросы, прежде чем обращаться к администрации веб-сервиса.
+            </p>
+          </Col>
+        </Row>{" "}
+        <Row>
+          <p className="question_title">
+            Частозадаваемые <span className="red">вопросы</span>
+          </p>
+        </Row>
+        <Row>
           <Accordion defaultActiveKey="0">
-
-            {
-                question?.map(item => (
-                    <Accordion.Item eventKey={item.id}>
-                    <Accordion.Header>{item.title}</Accordion.Header>
-                    <Accordion.Body>
-                        {
-                            item.information
-                        }
-                    </Accordion.Body>
-                  </Accordion.Item>
-                ))
-            }
-            </Accordion>
-        </Container>
-      </div>
-    </div>
+            {question?.map((item) => (
+              <Accordion.Item eventKey={item.id}>
+                <Accordion.Header>{item.title}</Accordion.Header>
+                <Accordion.Body>{item.information}</Accordion.Body>
+              </Accordion.Item>
+            ))}
+          </Accordion>
+        </Row>
+      </Container>
+    </>
   );
 }
